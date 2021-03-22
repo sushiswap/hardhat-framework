180c70bee7a5ad6d31f9ed3c29d40ac633b7bbbe
BoringCrypto <b@rtje.net>
2021-03-22 22:34:22 +1100

    Fix Bentobox version

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function getBentoBoxDomainSeparator(address, chainId) {
     return keccak256(
         defaultAbiCoder.encode(
             ["bytes32", "bytes32", "uint256", "address"],
-            [keccak256(toUtf8Bytes("EIP712Domain(string name,uint256 chainId,address verifyingContract)")), keccak256(toUtf8Bytes("BentoBox V2")), chainId, address]
+            [keccak256(toUtf8Bytes("EIP712Domain(string name,uint256 chainId,address verifyingContract)")), keccak256(toUtf8Bytes("BentoBox V1")), chainId, address]
         )
     )
 }
